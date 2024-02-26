dd86f439f7c6f924f64e1a64ab151a969a9f6279
DAVID DE ALMEIDA BEZERRA <68335111+davidbezerra405@users.noreply.github.com>
2024-02-26 16:09:03 -0300

    Adequação

--- app/src/main/java/com/example/quadrantes/MainActivity.kt
+++ app/src/main/java/com/example/quadrantes/MainActivity.kt
@@ -4,14 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -19,14 +17,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.VerticalAlignmentLine
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.quadrantes.ui.theme.QuadrantesTheme
-import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,7 +34,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting()
+                    QuadranteApp()
                 }
             }
         }
@@ -46,109 +42,73 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(modifier: Modifier = Modifier) {
+fun QuadranteApp() {
+    Column(Modifier.fillMaxWidth()) {
+        Row(Modifier.weight(1f)) {
+            QuadranteInfo(
+                titulo = stringResource(R.string.txt_q1_cab),
+                descricao = stringResource(R.string.txt_q1_texto),
+                backgroundColor = Color(0xFFEADDFF),
+                modifier = Modifier.weight(1f)
+            )
+            QuadranteInfo(
+                titulo = stringResource(R.string.txt_q2_cab),
+                descricao = stringResource(R.string.txt_q2_texto),
+                backgroundColor = Color(0xFFD0BCFF),
+                modifier = Modifier.weight(1f)
+            )
+        }
+        Row(
+            modifier = Modifier.weight(1f)
+        ) {
+            QuadranteInfo(
+                titulo = stringResource(R.string.txt_q3_cab),
+                descricao = stringResource(R.string.txt_q3_texto),
+                backgroundColor = Color(0xFFB69DF8),
+                modifier = Modifier.weight(1f)
+            )
+            QuadranteInfo(
+                titulo = stringResource(R.string.txt_q4_cab),
+                descricao = stringResource(R.string.txt_q4_texto),
+                backgroundColor = Color(0xFFF6EDFF),
+                modifier = Modifier.weight(1f)
+            )
+        }
+    }
+}
+
+@Composable
+private fun QuadranteInfo(
+    titulo: String,
+    descricao: String,
+    backgroundColor: Color,
+    modifier: Modifier = Modifier
+) {
     Column(
-        modifier = Modifier.fillMaxSize()
+        modifier = modifier
+            .background(backgroundColor)
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Row(
-            modifier = Modifier.weight(1f, fill = true)
-        ) {
-            Column(
-                modifier = Modifier
-                    .background(Color(0xFFEADDFF))
-                    .weight(weight = 1f, fill = true)
-                    .fillMaxSize()
-                    .padding(16.dp)
-                    .wrapContentSize(Alignment.Center)
-            ) {
-                Text(
-                    text = stringResource(R.string.txt_q1_cab),
-                    fontWeight = FontWeight.Bold,
-                    textAlign = TextAlign.Center,
-                    modifier = Modifier
-                        .padding(bottom = 16.dp),
-                )
-                Text(
-                    text = stringResource(R.string.txt_q1_texto),
-                    modifier = modifier,
-                    textAlign = TextAlign.Justify
-                )
-            }
-            Column(
-                modifier = Modifier
-                    .background(Color(0xFFD0BCFF))
-                    .weight(weight = 1f, fill = true)
-                    .fillMaxSize()
-                    .padding(16.dp)
-                    .wrapContentSize(Alignment.Center)
-            ) {
-                Text(
-                    text = stringResource(R.string.txt_q2_cab),
-                    fontWeight = FontWeight.Bold,
-                    textAlign = TextAlign.Center,
-                    modifier = Modifier
-                        .padding(bottom = 16.dp)
-                )
-                Text(
-                    text = stringResource(R.string.txt_q2_texto),
-                    modifier = modifier,
-                    textAlign = TextAlign.Justify
-                )
-            }
-        }
-        Row(
-            modifier = Modifier.weight(1f, fill = true)
-        ) {
-            Column(
-                modifier = Modifier
-                    .background(Color(0xFFB69DF8))
-                    .weight(weight = 1f, fill = true)
-                    .fillMaxSize()
-                    .padding(16.dp)
-                    .wrapContentSize(Alignment.Center)
-            ) {
-                Text(
-                    text = stringResource(R.string.txt_q3_cab),
-                    fontWeight = FontWeight.Bold,
-                    textAlign = TextAlign.Center,
-                    modifier = Modifier
-                        .padding(bottom = 16.dp)
-                )
-                Text(
-                    text = stringResource(R.string.txt_q3_texto),
-                    modifier = modifier,
-                    textAlign = TextAlign.Justify
-                )
-            }
-            Column(
-                modifier = Modifier
-                    .background(Color(0xFFF6EDFF))
-                    .weight(weight = 1f, fill = true)
-                    .fillMaxSize()
-                    .padding(16.dp)
-                    .wrapContentSize(Alignment.Center)
-            ) {
-                Text(
-                    text = stringResource(R.string.txt_q4_cab),
-                    fontWeight = FontWeight.Bold,
-                    textAlign = TextAlign.Center,
-                    modifier = Modifier
-                        .padding(bottom = 16.dp)
-                )
-                Text(
-                    text = stringResource(R.string.txt_q4_texto),
-                    modifier = modifier,
-                    textAlign = TextAlign.Justify
-                )
-            }
-        }
+        Text(
+            text = titulo,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+        Text(
+            text = descricao,
+            textAlign = TextAlign.Justify
+        )
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun QuadrantePreview() {
     QuadrantesTheme {
-        Greeting()
+        QuadranteApp()
     }
 }
